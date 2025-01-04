c1a254096c45ed1559fdeecffc9ceee092dd59dc
RuneMagus <72150088+RuneMagus@users.noreply.github.com>
2025-01-04 08:34:56 +0000

    Update streamlit_app.py
    Fix session

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.title(":cup_with_straw: My Parents :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
@@ -12,7 +12,8 @@ st.write(
 name_on_order = st.text_input("Movie title", "")
 st.write("Name on Smoothie:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients', 
