1583526d6fc2f21694ec6c2a4f6befd5a7c89506
RuneMagus <72150088+RuneMagus@users.noreply.github.com>
2025-01-04 09:07:50 +0000

    Update streamlit_app.py
    Extract response data and display in grid

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,5 @@ if ingredients_list:
 # new section to display smootiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
